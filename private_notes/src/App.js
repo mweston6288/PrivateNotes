9eb0109db7da097c900f47142664166210014a5f
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-09-18 15:36:05 -0400

    Added eslint for formatting and did an early test. Setup is ready to go

--- private_notes/src/App.js
+++ private_notes/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		// Main page contains two columns. The left column will display saved notes.
+		// The right will let users make new notes
+		<Container>
+			<Row>
+				<Col>
+				</Col>
+				<Col>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
