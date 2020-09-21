85943e863f134bbd175d8d14104b00a32939697b
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-09-21 17:51:29 -0400

    Rewrote the NewNotes component to be more object structured. Added a state tracker to it. Component is confirmed to update successfully with user input

--- private_notes/src/components/NewNote.js
+++ private_notes/src/components/NewNote.js
@@ -1,15 +1,37 @@
+/*
+	Component for new user notes. This component will load and be usable regardless
+	of if the user is logged in. Edits to the title and body are saved in a 
+	state that will be passed along if the user saves the note
+*/
+
 import React from "react";
 import Form from "react-bootstrap/Form";
 
-function NewNote(){
-	return(
-		<Form>
-			<Form.Group>
-				<Form.Control size="lg" type="text" placeholder="Title" />
-				<Form.Control as="textarea" rows="5" />
-			</Form.Group>
-		</Form >
-	);
-
+class NewNote extends React.Component{
+	state = {
+		Title:"",
+		Body:""
+	};
+	// On changing the value in either text box, these two methods handle updating
+	// the state
+	handleTitleChange = (event) => {
+		this.setState({ ...this.state, Title: event.target.value });
+		console.log(this.state);
+	}
+	handleBodyChange = (event)=>{
+		this.setState({...this.state,Body:event.target.value});
+		console.log(this.state);
+	}
+	// Load component. Uses Bootstrap's Form Component
+	render(){
+		return (
+			<Form>
+				<Form.Group>
+					<Form.Control size="lg" type="text" placeholder="Title" onChange={this.handleTitleChange} />
+					<Form.Control as="textarea" rows="5" onChange={this.handleBodyChange}/>
+				</Form.Group>
+			</Form >
+		);
+	}
 }
 export default NewNote;
