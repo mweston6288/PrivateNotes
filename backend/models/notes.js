51db4e5e16fa26db98a059126deed05381b47393
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-09-28 11:33:06 -0400

    Still working on database things. I reworked the model to better match what information I could find about it. I'll next need to work on routing

--- backend/models/notes.js
+++ backend/models/notes.js
@@ -1,15 +1,29 @@
 /*
 	Data model for saved notes. Notes contain an id, Title, Body, and owner
 */
+const Sequelize = require ("sequelize");
+const sequelize = new Sequelize("notes", "root", "", {
+	host: "localhost",
+	port: 3306,
+	dialect: "mysql"
+});
 
-module.exports = function(sequelize, DataTypes) {
-	const Notes = sequelize.define("Notes",{
-		id: DataTypes.NUMBER,
-		Title: DataTypes.STRING,
-		Body: DataTypes.STRING
-	});
+
+
+const Notes = sequelize.define("Notes",{
+	id: {
+		type: DataTypes.NUMBER,
+		autoincrement: true
+	},
+	Title: DataTypes.TEXT,
+	Body: DataTypes.TEXT,
+	Date: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW
+	}
+});
 
 	// TODO: associate notes with owners
+Notes.sync();
 
-	return Notes;
-}
+module.exports = Notes;
