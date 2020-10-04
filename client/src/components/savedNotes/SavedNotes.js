d551cef7855921d116f1ce7c8280d577f846287a
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-10-04 14:09:18 -0400

    Created component that displays individual notes. It is ugly but it works. Next up, I need to update saved notes when a new note is added

--- client/src/components/savedNotes/SavedNotes.js
+++ client/src/components/savedNotes/SavedNotes.js
@@ -1,18 +1,28 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Note from "./Note.js"
 import axios from "axios";
 
 class SavedNotes extends React.Component {
-	
+	state = {
+		notes:[]
+	}
 	componentDidMount(){
 		axios.get("/api/notes").then(response=>{
 			console.log(response);
+			this.setState({ ...this.state, notes: response.data});
+			console.log(this.state);
+
 		})
 	}
 	render(){
 		return(
 			<Container>
-
+				<div>
+					{this.state.notes.map((note)=>(
+						<Note Title={note.Title} Body={note.Body}/>
+					))}
+				</div>
 			</Container>
 		)
 	}
